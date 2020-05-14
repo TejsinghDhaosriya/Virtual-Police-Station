6b0126edaabcfe363ac09caf8af8b422aaf5c76c
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-05-14 20:53:08 +0530

    xyz

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ process.on('SIGINT', function(){
  |  |   |   |   |   |   |   |   |   INDEX ROUTE
 *********************************************************************************************************************/
 expobj.get("/",function(req,res) {
-    res.render("server");    
+    res.render("verifyOTP");    
 });
 
 expobj.use("/user",userRoutes);
