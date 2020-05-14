4f4b2204da17881600db61022d0cad9b7c0e6782
Mayuri Birle <mayuri.birle09@gmail.com>
2020-05-14 19:27:35 +0530

    changes have been made

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,31 @@ const router  = express.Router();
 
 
 router.get("/login",function(req,res){
-    res.render("login");
+    res.render("login",{type:"user"});
 });
 
 router.get("/sign_up", async function(req,res) {
-    console.log('/register');
+    console.log('/register',{type:"user"});
     // const emailExist    
 })
+router.post("/user/login",async function(req,res){
+    console.log("user/login")
+    const user = await User.findOne({email:req.body.email});
+    if(!user){
+        res.send("email does't exist")
+    }else{
+        const validpass =await bcrypt.compare(req.body.password,user.password)
+        if(!validpass){
+            res.send("Invalid password")
+        }else{
+            const token = jwt.sign({_id:user._id},process.env.TOKEN_SECRET);
+            res.cookie('authToken',token,{
+                maxAge:2628000000, //1 month in mili sec
+                httpOnly:true
+            });
+            res.redirect("/main");
+        }
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,78 +2,88 @@
 var express = require('express');
 var expobj = express();
 var bodyparser = require('body-parser');
+var bcrypt = require("bcrypt")
+var jwt = require("jsonwebtoken");
 var methodOverride = require('method-override');
+
+
 var mongoose = require('mongoose');
 var cookieParser = require("cookie-parser");
 const path = require('path');
+var dotenv = require("dotenv");
+dotenv.config()
 /* This code you have to validate */
-const accountSid = '';
-const authToken = '';
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = '';
+// const authToken = '';
+// const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'Your Verification Code is ' + generateOTP(),
-     from: '+12058800634',
-     to: '+919039102681'
-   })
-  .then(message => console.log(message.sid));
+// client.messages
+//   .create({
+//      body: 'Your Verification Code is ' + generateOTP(),
+//      from: '+12058800634',
+//      to: '+919039102681'
+//    })
+//   .then(message => console.log(message.sid));
 
-  function generateOTP()
-  {
-  
-      var digits = '0123456789';
-  
-      var otpLength = 4;
-  
-      var otp = '';
-  
-      for(let i=1; i<=otpLength; i++)
-  
-      {
-  
-          var index = Math.floor(Math.random()*(digits.length));
-  
-          otp = otp + digits[index];
-  
-      }
-  
-      return otp;
-  
-  }
+function generateOTP() {
+
+    var digits = '0123456789';
+
+    var otpLength = 4;
+
+    var otp = '';
+
+    for (let i = 1; i <= otpLength; i++)
+
+    {
+
+        var index = Math.floor(Math.random() * (digits.length));
+
+        otp = otp + digits[index];
+
+    }
+
+    return otp;
+
+}
 /************************************************************* */
 // expobj.use(require('./routes'));
 // requiring routes
 var userRoutes = require("./routes/user");
 var policeRoutes = require("./routes/police");
-var adminRoutes  = require("./routes/admin");
+var adminRoutes = require("./routes/admin");
 
 //set port
 var port = process.env.PORT || 8080;
 //parse application/json
 expobj.use(bodyparser.json());
 //parse aplication/x-www url encoded 
-expobj.use(bodyparser.urlencoded({extended:true}));
+expobj.use(bodyparser.urlencoded({
+    extended: true
+}));
 //method-override
 expobj.use(methodOverride('X-HTTP-METHOD-OVERRIDE'));
-expobj.set("view engine","ejs");
+expobj.set("view engine", "ejs");
 expobj.engine('ejs', require('ejs').__express);
 
-expobj.use(express.static(path.join(__dirname,"public")));
-mongoose.connect("mongodb://localhost/users",{useNewUrlParser: true, useUnifiedTopology: true});
+expobj.use(express.static(path.join(__dirname, "public")));
+mongoose.connect("mongodb://localhost/police_station", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 //on connection
-mongoose.connection.on('connected',function(){
+mongoose.connection.on('connected', function () {
     console.log('connected to database mongoDB @ 27017');
 });
-mongoose.connection.on('err', function(err){
-    if(err){
-        console.log('error in database connection:'+err);
+mongoose.connection.on('err', function (err) {
+    if (err) {
+        console.log('error in database connection:' + err);
     }
 });
-mongoose.connection.on('disconnected' , function (){
+mongoose.connection.on('disconnected', function () {
     console.log('disconnected from databasee monngoDB ');
 });
-process.on('SIGINT', function(){
+process.on('SIGINT', function () {
     console.log('Disconnected from database mongoDB through app termination');
     process.exit(0);
 });
@@ -82,11 +92,11 @@ process.on('SIGINT', function(){
 /*******************************************************************************************************************
  |  |   |   |   |   |   |   |   |   INDEX ROUTE
 *********************************************************************************************************************/
-expobj.get("/",function(req,res) {
-    res.render("sign_up");    
+expobj.get("/", function (req, res) {
+    res.render("index");
 });
 
-expobj.use("/user",userRoutes);
+expobj.use("/user", userRoutes);
 // expobj.use("/admin",adminRoutes);
 // expobj.use("/police",policeRoutes);
 
@@ -95,6 +105,5 @@ expobj.use("/user",userRoutes);
 expobj.listen(port);
 //infrom to the user
 console.log('Node server has been started');
-console.log('\nTo check it pen any web browser and type "localhost:'+port+'"');
+console.log('\nTo check it pen any web browser and type "localhost:' + port + '"');
 exports = module.exports = expobj;
-
