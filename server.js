79f604e9daa8854d99c2202177f5030f881c8ba1
Mayuri Birle <mayuri.birle09@gmail.com>
2020-06-16 13:29:22 +0530

    removed unnecessary files

--- server.js
+++ server.js
@@ -42,12 +42,6 @@ process.on('SIGINT', function () {
 });
 
 
-// app.get("/", function (req, res) {
-//     res.render("index");
-// });
-// app.get("/sign_up", function (req, res) {
-//     res.render("sign_up",{type: "user"});
-// });
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", index);
 app.use("/user", user);
@@ -55,62 +49,3 @@ app.listen(port);
 console.log('Node server has been started');
 console.log('\nTo check it pen any web browser and type "localhost:' + port + '"');
 exports = module.exports = app;
-
-
-
-// app.post("/sign_up", async (req, res) => {
-//     // res.redirect("/auth/567");
-//     const user = await User.findOne({
-//         MobileNo: req.body.number
-//     });
-//     console.log("sign_up")
-//     if (!user) {
-//         console.log("user")
-//         const client = require('twilio')('', '')
-//         const otp = generateOTP();
-//         console.log(otp)
-//         client.messages
-//             .create({
-//                 body: 'Your Verification Code is ' + otp,
-//                 from: '+12058800634',
-//                 to: '+919039102681'
-//             })
-//             .then(message => console.log(message.sid));
-
-//         function generateOTP() {
-//             var digits = '0123456789';
-//             var otpLength = 4;
-//             var otp = '';
-//             for (let i = 1; i <= otpLength; i++) {
-//                 var index = Math.floor(Math.random() * (digits.length));
-//                 otp = otp + digits[index];
-//             }
-//             return otp;
-//         }
-
-//         var user1 = new User({
-//             FirstName: req.body.first,
-//             LastName: req.body.last,
-//             MobileNo: req.body.number,
-//             Email: req.body.email,
-//             Gender: req.body.gender,
-//             IDType: req.body.idtype,
-//             CurrentAddress: req.body.village,
-//             IDNo: req.body.idno,
-//             State: req.body.state,
-//             District: req.body.district,
-//             Zip:req.body.zip,
-//             LoginID: req.body.login,
-//             Password: req.body.password,
-//             otp: otp + "",
-//             verified: false
-//         });
-//         console.log(req.body);
-//         const result = await user1.save();
-//         console.log(result)
-//         res.redirect("/auth/" + req.body.number);
-//     } else {
-//         res.send("Phone No already exist..")
-//     }
-// });
-// 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
